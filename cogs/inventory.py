0e07f9a7618ea313f8d6f14a0244e6eebbde4522
rooni <nekorooni@gmail.com>
2017-10-23 19:04:25 +0200

    Add item data to inventory command

--- cogs/inventory.py
+++ cogs/inventory.py
@@ -6,16 +6,18 @@ import random
 
 from cogs.profiles import needs_profile
 
+
 def has_item(**kwargs):
     async def predicate(ctx):
         if ctx.guild is None:
             return False
         kwargs['profile_id'] = ctx.profile.pid
         items = await ctx.bot.db.fetch(f'SELECT * FROM `inventory` '
-                                       f'WHERE '+' AND '.join([f'{k}={v}' for k, v in kwargs.items()]))
+                                       f'WHERE '+' AND '.join([f'{k}="{v}"' for k, v in kwargs.items()]))
 
         return len(items) > 0
     return commands.check(predicate)
+
 
 class Inventory:
     """Inventory stuff"""
@@ -26,9 +28,9 @@ class Inventory:
     @commands.group(aliases=['inv'])
     @needs_profile()
     async def inventory(self, ctx):
-        items = await ctx.bot.db.fetch(f'SELECT it.name, it.shortdesc FROM inventory '
+        items = await ctx.bot.db.fetch(f'SELECT it.name, it.shortdesc, data FROM inventory '
                                        f'INNER JOIN items it ON item_id=it.id WHERE profile_id={ctx.profile.pid}')
-        await ctx.send('\n'.join([f'{name} - {short}' for name, short in items]))
+        await ctx.send('\n'.join([f'{name}{" "+data if data else ""} - {short}' for name, short, data in items]))
 
     @commands.command()
     @needs_profile()
