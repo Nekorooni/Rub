ef1edd27012c615db5b1755ca8b4dc3b6d2d2308
rooni <nekorooni@gmail.com>
2017-10-23 18:14:07 +0200

    Add inventory cog

---
+++ cogs/inventory.py
@@ -0,0 +1,40 @@
+import aiohttp
+from discord.ext import commands
+
+import discord
+import random
+
+from cogs.profiles import needs_profile
+
+def has_item(**kwargs):
+    async def predicate(ctx):
+        if ctx.guild is None:
+            return False
+        kwargs['profile_id'] = ctx.profile.pid
+        items = await ctx.bot.db.fetch(f'SELECT * FROM `inventory` '
+                                       f'WHERE '+' AND '.join([f'{k}={v}' for k, v in kwargs.items()]))
+
+        return len(items) > 0
+    return commands.check(predicate)
+
+class Inventory:
+    """Inventory stuff"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(aliases=['inv'])
+    @needs_profile()
+    async def inventory(self, ctx):
+        items = await ctx.bot.db.fetch(f'SELECT it.name, it.shortdesc FROM inventory '
+                                       f'INNER JOIN items it ON item_id=it.id WHERE profile_id={ctx.profile.pid}')
+        await ctx.send('\n'.join([f'{name} - {short}' for name, short in items]))
+
+    @commands.command()
+    @needs_profile()
+    @has_item(id=1)
+    async def use(self, ctx):
+        await ctx.send('All gud!')
+
+def setup(bot):
+    bot.add_cog(Inventory(bot))
