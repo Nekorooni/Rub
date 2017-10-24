f44c9cbca9184f2f5c09ab631bb413c6d6da1db1
rooni <nekorooni@gmail.com>
2017-10-24 18:52:47 +0200

    Add role color test commands

--- cogs/Helpful.py
+++ cogs/Helpful.py
@@ -11,6 +11,13 @@ from discord.ext import commands
 import discord
 import random
 
+
+def get_color_role(member):
+    for role in member.roles:
+        if role.color == member.color:
+            return role
+
+
 class Helpful:
     """Random helpful commands"""
 
@@ -28,6 +35,17 @@ class Helpful:
     async def ping(self, ctx):
         t = datetime.datetime.utcnow() - ctx.message.created_at
         await ctx.send(f'pong :v *{t.total_seconds()}*')
+
+    @commands.command()
+    async def color(self, ctx, color: discord.Colour):
+        if ctx.guild.id != 352672918473277440:
+            return
+        role = await ctx.guild.create_role(name=str(color), colour=color)
+        await ctx.author.remove_roles(get_color_role(ctx.author))
+        await ctx.author.add_roles(role)
+        await ctx.guild.me.add_roles(role)
+        await ctx.guild.me.remove_roles(get_color_role(ctx.guild.me))
+        await ctx.send(str(color))
 
     @commands.command()
     async def get_active(self, ctx, limit=100):
