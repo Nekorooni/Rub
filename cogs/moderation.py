260c8b440706870176372e045f27853e991404af
rooni <nekorooni@gmail.com>
2017-11-25 04:32:22 +0100

    Update moderation commands

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -4,6 +4,37 @@ import asyncio
 import discord
 import random
 
+class MemberID(commands.Converter):
+    async def convert(self, ctx, argument):
+        try:
+            m = await commands.MemberConverter().convert(ctx, argument)
+        except commands.BadArgument:
+            try:
+                return int(argument, base=10)
+            except ValueError:
+                raise commands.BadArgument(f"{argument} is not a valid member or member ID.") from None
+        else:
+            can_execute = ctx.author.id == ctx.bot.owner_id or \
+                          ctx.author == ctx.guild.owner or \
+                          ctx.author.top_role > m.top_role
+
+            if not can_execute:
+                raise commands.BadArgument('You cannot do this action on this user due to role hierarchy.')
+            return m.id
+
+class BannedMember(commands.Converter):
+    async def convert(self, ctx, argument):
+        ban_list = await ctx.guild.bans()
+        try:
+            member_id = int(argument, base=10)
+            entity = discord.utils.find(lambda u: u.user.id == member_id, ban_list)
+        except ValueError:
+            entity = discord.utils.find(lambda u: str(u.user) == argument, ban_list)
+
+        if entity is None:
+            raise commands.BadArgument("Not a valid previously-banned member.")
+        return entity
+
 class Moderation:
     """Moderation commands"""
 
@@ -21,31 +52,32 @@ class Moderation:
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, member: discord.Member, *, reason=None):
+    async def ban(self, ctx, member: MemberID, *, reason=None):
         try:
-            await ctx.guild.ban(member, reason=reason)
+            await ctx.guild.ban(discord.Object(id=member), reason=reason)
             await ctx.message.add_reaction('hqcaramel:361532992499220492')
         except:
             await ctx.message.add_reaction('tsumikuBlank:364178887917436933')
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def softban(self, ctx, member: discord.Member, *, reason=None):
+    async def softban(self, ctx, member: MemberID, *, reason=None):
         try:
-            await ctx.guild.ban(member, reason=reason)
-            await ctx.guild.unban(member, reason='Softban')
+            await ctx.guild.ban(discord.Object(id=member), reason=reason)
+            await ctx.guild.unban(discord.Object(id=member), reason=reason)
             await ctx.message.add_reaction('hqcaramel:361532992499220492')
         except:
             await ctx.message.add_reaction('tsumikuBlank:364178887917436933')
 
     @commands.command()
-    @commands.has_permissions(manage_messages=True)
-    async def purge(self, ctx, *params):
-        data={}
-        for k in iter(params):
-            data[k] = discord.Object(id=next(k))
-        await ctx.channel.purge(limit=100, **data)
-
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, member: BannedMember, *, reason=None):
+        try:
+            await ctx.guild.unban(member.user, reason=reason)
+            await ctx.message.add_reaction('hqcaramel:361532992499220492')
+        except Exception as e:
+            await ctx.send(e)
+            await ctx.message.add_reaction('tsumikuBlank:364178887917436933')
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
