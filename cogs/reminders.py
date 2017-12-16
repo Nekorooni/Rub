f5c4ddf1f8b6e89f130919a91a39c43b45fac496
rooni <nekorooni@gmail.com>
2017-12-16 02:29:15 +0100

    Update reminders.py to use new timers cog

--- cogs/reminders.py
+++ cogs/reminders.py
@@ -1,4 +1,5 @@
 import asyncio
+import datetime
 
 import parsedatetime as pdt
 from discord.ext import commands
@@ -6,15 +7,14 @@ from discord.ext import commands
 
 def get_date(text):
     cal = pdt.Calendar()
-    time, res = cal.parseDT(text)
+    time, res = cal.parseDT(text, datetime.datetime.utcnow())
     return time if res else None
 
 class Reminders:
     """Tools for reminding me"""
     def __init__(self, bot):
         self.bot = bot
-        self.reminders = []
-        self.bot.loop.create_task(self.remind_check_loop())
+        self.timers = bot.get_cog('Timers')
 
     async def on_message(self, msg):
         if msg.content.lower().startswith('remind '):
@@ -31,29 +31,22 @@ class Reminders:
                                               check=lambda m: m.author == msg.author and m.channel == msg.channel)
                 time = get_date(msg.content)
             if time:
-                if await self.make_reminder(msg.author.id, reminder, time):
-                    await msg.channel.send(f"I'll remind you at {time}")
-                else:
-                    await msg.channel.send(f"There was a problem setting your reminder.")
+                await self.timers.create_timer('reminder', time, [msg.channel.id, reminder])
+                await msg.channel.send(f'I\'ll remind you then!')
             else:
                 await msg.channel.send(f"Idk when you want me to remind you")
 
-    async def make_reminder(self, author_id, note, due):
-        try:
-            await self.bot.db.execute(f'INSERT INTO reminders (author, note, due) VALUES ({author_id}, "{note}", "{due}")')
-            return True
-        except Exception as e:
-            print(e)
-            return False
+    async def on_reminder_event(self, destination, msg):
+        channel = self.bot.get_channel(destination)
+        if channel is None:
+            # Check if it's a DM channel
+            author = self.bot.get_user(destination)
+            try:
+                channel = await author.dm_channel()
+            except:
+                return
 
-    async def remind_check_loop(self):
-        await self.bot.wait_until_ready()
-        while not self.bot.is_closed():
-            await asyncio.sleep(60)
-            reminders = await self.bot.db.fetch('SELECT id, author, note FROM reminders WHERE NOT done AND due<NOW()')
-            for _id, author, note in reminders:
-                await self.bot.db.execute(f'UPDATE reminders SET done=1 WHERE id={_id}')
-                await self.bot.get_user(author).send(f"**I had to remind you:**\n{note} <:blobcaramel:325504959544295424>")
+        await channel.send(f'{msg}')
 
 def setup(bot):
     bot.add_cog(Reminders(bot))
