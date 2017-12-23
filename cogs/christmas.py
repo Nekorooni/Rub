a05f8d8d6aaa89b5ea19a3da1b70ddb697e8840b
rooni <nekorooni@gmail.com>
2017-12-23 19:00:22 +0100

    Testing new formula

--- cogs/christmas.py
+++ cogs/christmas.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 import random
+from collections import deque
 
 import discord
 import math
@@ -44,22 +45,46 @@ class Christmas:
     @lotto.command(name='winners')
     @commands.has_any_role('Admin')
     async def lotto_winners(self, ctx):
-        entries = [ctx.guild.get_member(x) for x, in await self.bot.db.fetch("SELECT user_id FROM lotto_entries")]
+        entries = deque([ctx.guild.get_member(x) for x, in await self.bot.db.fetch("SELECT user_id FROM lotto_entries")])
+        winners = [ctx.guild.get_member(x) for x, in await self.bot.db.fetch("SELECT user_id FROM lotto_winners")]
+
         random.shuffle(entries)
+        for i in range(len(entries)):
+            if entries[0].id in winners:
+                entries.rotate(-1)
+                print(f'{entries[0]} won so rotating')
+            else:
+                break
+
+        rewards = {
+            '<:goldball:390204323759652866> Gold ball': .10,
+            '<:purpleball:390204323960848386> Purple ball': .10,
+            '🍏 Green apple ball': .25
+        }
+
         output = "**Winners of this round:**"
         await ctx.send(output)
+
         # Gold ball (1 member)
         output = "<:goldball:390204323759652866> Gold ball\n"
-        output += entries[0].mention+'\n<:space:390204819979370508>'
+        output += entries[0].mention + '\n<:space:390204819979370508>'
         await ctx.send(output)
+
         # Purple ball (10% of members)
-        n = math.ceil(len(entries)/10)
+        n = math.ceil(len(entries) * .10) + 1
         output = "<:purpleball:390204323960848386> Purple ball\n"
-        output += ', '.join([x.mention for x in entries[1:n+1]])+'\n<:space:390204819979370508>'
+        output += ', '.join([x.mention for x in entries[1:n]]) + '\n<:space:390204819979370508>'
         await ctx.send(output)
-        # Red ball (10% of members)
+
+        # Green ball (25% of members)
+        n2 = math.ceil(len(entries) *.25)
+        output = "🍏 Green apple ball\n"
+        output += ', '.join([x.mention for x in entries[n:n + n2 + 1]]) + '\n<:space:390204819979370508>'
+        await ctx.send(output)
+
+        # Red ball
         output = "<:redball:390204324329816074> Red ball\n"
-        output += ', '.join([x.mention for x in entries[n+1:]])+'\n<:space:390204819979370508>'
+        output += ', '.join([x.mention for x in entries[n + n2 + 1:]]) + '\n<:space:390204819979370508>'
         await ctx.send(output)
         output = 'Check <#393160210166054912> for your reward options and ping a mod with what you want! In the case of gold ball, dm <@211238461682876416>.'
         await ctx.send(output)
