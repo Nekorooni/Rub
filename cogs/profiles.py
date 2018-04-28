2640d72ef8e950c1fd594996f0ea5c2d5697570a
rooni <nekorooni@gmail.com>
2018-04-28 20:40:40 +0200

    Update profiles

--- cogs/profiles.py
+++ cogs/profiles.py
@@ -1,8 +1,8 @@
 import datetime
 import random
-from collections import defaultdict
 
 import aiohttp
+import asyncio
 from discord.ext import commands
 import discord
 
@@ -11,6 +11,7 @@ EXCLUDED_CHANNELS = [
     371046668856197131,  # Snackbox
     370941450361372672,  # Yui-Roo
 ]
+
 
 def exp_needed(level):
     return round(20 * level ** 1.05)
@@ -25,6 +26,9 @@ def needs_profile(keys=None):
         if ctx.guild is None:
             return False
 
+        if ctx.invoked_with.lower() == 'help':
+            return True
+
         cog = ctx.bot.get_cog('Profiles')
         async with ctx.typing():
             ctx.profile = await cog.get_profile(ctx.author.id, keys)
@@ -33,18 +37,21 @@ def needs_profile(keys=None):
     return commands.check(predicate)
 
 
-class PartialProfile:
-    __slots__ = ('pid', 'level', 'experience', 'coins')
+class Profile:
+    __slots__ = ('user_id', 'level', 'experience', 'fame', 'coins')
 
-    def __init__(self, pid, **kwargs):
-        self.pid = pid
-        for k in kwargs:
-            setattr(self, k, kwargs[k])
+    def __init__(self, uid, **kwargs):
+        self.user_id = uid
+        self.level = kwargs.get('level', 1)
+        self.experience = kwargs.get('experience', 1)
+        self.coins = kwargs.get('coins', 1)
 
     async def save(self, db):
-        s = ', '.join([f'{s}={getattr(self,s,None)}' for s in self.__slots__[1:] if getattr(self, s, None) is not None])
-        await db.execute(f"UPDATE profiles SET {s} WHERE id={self.pid}")
+        s = ','.join([f'{s}={getattr(self,s,None)}' for s in self.__slots__[1:] if getattr(self, s, None) is not None])
+        await db.execute(f"UPDATE profiles SET {s} WHERE user_id={self.user_id}")
 
+    async def has_item(self, name=None):
+        pass
 
 class Profiles:
     """Stuff for profiles"""
@@ -52,53 +59,62 @@ class Profiles:
     def __init__(self, bot):
         self.bot = bot
         self.cooldowns = {}
-        self.profiles = {}
+        self._lock = asyncio.Lock(loop=bot.loop)
 
-    async def get_profile(self, u_id, keys=None):
-        if u_id in self.profiles:
-            p_id = self.profiles[u_id]
-        else:
-            p_id, = await self.bot.db.fetchone(f'SELECT id FROM profiles WHERE user_id={u_id}') or (None,)
-            if not p_id:
-                _, p_id = await self.bot.db.execute(f'INSERT INTO profiles (user_id) VALUES ({u_id})')
-            self.profiles[u_id] = p_id
-        if keys:
-            s = ', '.join(keys)
-            data = await self.bot.db.fetchdict(f'SELECT {s} FROM profiles WHERE id={p_id}')
-            return PartialProfile(p_id, **data)
-        return PartialProfile(p_id)
+    async def get_profile(self, uid, keys=None):
+        s = ', '.join(keys)
+        profile = await self.bot.db.fetchdict(f'SELECT {s or "*"} FROM profiles WHERE user_id={uid}')
+        if not profile:
+            await self.bot.db.execute(f'INSERT INTO profiles (user_id, coins) VALUES (%s, %s)', (uid, 500))
+            return Profile(uid, level=1, experience=0, coins=500)
+        return Profile(uid, **profile)
 
     async def on_message(self, msg):
         if not msg.guild:
-            return
-        if msg.guild.id != 320567296726663178:
             return
         if msg.channel.id in EXCLUDED_CHANNELS:
             return
         if msg.author.bot:
             return
-        profile = await self.get_profile(msg.author.id, ('level', 'experience', 'coins'))
-        d = abs(msg.created_at - self.cooldowns.get(profile.pid, datetime.datetime(2000, 1, 1)))
-        if d < datetime.timedelta(seconds=20):
-            return
-        profile.experience += 10
-        profile.coins += 1
-        needed = exp_needed(profile.level)
-        if profile.experience >= needed:
-            profile.level += 1
-            profile.experience -= needed
-            if profile.level == 3:
-                await msg.author.add_roles(discord.utils.get(msg.guild.roles, name='Neko'))
-        await profile.save(self.bot.db)
-        self.cooldowns[profile.pid] = msg.created_at
+
+        async with self._lock:
+            profile = await self.get_profile(msg.author.id, ('level', 'experience', 'coins'))
+
+            d = abs(msg.created_at - self.cooldowns.get(profile.user_id, datetime.datetime(2000, 1, 1)))
+            if d < datetime.timedelta(seconds=30):
+                return
+
+            profile.experience += 10
+            needed = exp_needed(profile.level)
+
+            # Terrible temporary levelup
+            if profile.experience >= needed:
+                profile.level += 1
+                profile.experience -= needed
+                profile.coins += random.randint(1, 10)
+
+            await profile.save(self.bot.db)
+            self.cooldowns[profile.user_id] = msg.created_at
 
     @commands.command()
-    async def xp(self, ctx, member: discord.Member=None):
+    async def profile(self, ctx, member: discord.Member = None):
         if not member:
             member = ctx.author
         p = await self.get_profile(member.id, ('level', 'experience'))
-        em = discord.Embed(title=f'**{member}**',
+        em = discord.Embed(title=f'{member}',
                            description=f'**Lv{p.level}** {p.experience}/{exp_needed(p.level)}xp')
+        await ctx.send(embed=em)
+
+    @commands.command(aliases=['experience'])
+    async def xp(self, ctx, member: discord.Member=None):
+        """Shows your exp"""
+        if not member:
+            member = ctx.author
+        p = await self.get_profile(member.id, ('level', 'experience'))
+        em = discord.Embed(title=f'{member.display_name}', colour=0x77dd77)
+        em.add_field(name="Level", value=f'**{p.level}**')
+        em.add_field(name="Exp", value=f'{p.experience}/{exp_needed(p.level)}xp')
+        em.set_thumbnail(url=member.avatar_url_as(size=64))
         await ctx.send(embed=em)
 
     @commands.group(invoke_without_command=True)
@@ -132,28 +148,27 @@ class Profiles:
         else:
             await ctx.send(f"{member} would be in debt.")
 
-    @commands.command()
-    @needs_profile()
-    async def inv2(self, ctx):
-        items = await ctx.bot.db.fetch(f'SELECT it.name, it.shortdesc, data FROM inventory '
-                                       f'INNER JOIN items it ON item_id=it.id WHERE profile_id={ctx.profile.pid} '
-                                       f'LIMIT 10')
-        embeds = [discord.Embed.from_data({'title': f'{n} `{d}`', 'color': random.randrange(1, 16777215)}) for n, s, d in items]
-        async with aiohttp.ClientSession() as session:
-            hook = discord.Webhook.from_url('https://canary.discordapp.com/api/webhooks/380881003159486465/hF1rfRXZYReBP2WjgavnrYHQuXKU6joN4aBPsfsaWszqUzFY5I8RmwMyIhGDG2eOH-n5', adapter=discord.AsyncWebhookAdapter(session))
-            await hook.send(embeds=embeds)
-
     @commands.group(aliases=['inv'])
-    @needs_profile()
     async def inventory(self, ctx, member: discord.Member = None):
         if member:
             ctx.profile = await self.get_profile(member.id)
         items = await ctx.bot.db.fetch(f'SELECT it.name, data FROM inventory '
-                                       f'INNER JOIN items it ON item_id=it.id WHERE profile_id={ctx.profile.pid}')
+                                       f'INNER JOIN items it ON item_id=it.id '
+                                       f'WHERE user_id=%s', (ctx.author.id,))
         if items:
             await ctx.send('\n'.join([f'{data+" " if data else ""}{name}' for name, data in items]))
         else:
             await ctx.send("You don't have anything.")
+
+    @commands.command(aliases=['color'])
+    async def colour(self, ctx, *, colour):
+        """Change your color using colour coupons"""
+        role = discord.utils.get(ctx.guild.roles, name=colour)
+
+        if role:
+            pass
+        else:
+            await ctx.send("I don't know that colour. Did you type it right?")
 
 
 def setup(bot):
