ad2b9b6b2fde7b3f68da7eb8b587020261b8c202
Rooni <nekorooni@gmail.com>
2018-06-12 16:30:52 +0200

    Update gitignore and use env vars

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+token = os.getenv('RUB_TOKEN')
+
+prefix = ['!!', '``']
+
+cogs = os.getenv('RUB_COGS', 'cogs.admin').split()
+
+db_host = os.getenv('MYSQL_HOST', '127.0.0.1')
+db_user = os.getenv('MYSQL_USER', 'root')
+db_pass = os.getenv('MYSQL_PASS', '')
+db_name = os.getenv('MYSQL_DATABASE', 'rub')
