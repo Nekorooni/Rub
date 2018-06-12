25171da01c616a83676441cc77a8f865a6bbfb38
Rooni <nekorooni@gmail.com>
2018-06-12 16:50:41 +0200

    Fix password env

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ cogs = os.getenv('RUB_COGS', 'cogs.admin').split()
 
 db_host = os.getenv('MYSQL_HOST', '127.0.0.1')
 db_user = os.getenv('MYSQL_USER', 'root')
-db_pass = os.getenv('MYSQL_PASS', '')
+db_pass = os.getenv('MYSQL_PASSWORD', '')
 db_name = os.getenv('MYSQL_DATABASE', 'rub')
